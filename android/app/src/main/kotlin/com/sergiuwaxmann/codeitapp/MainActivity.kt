67df1d1dd24b5e85bc2132db0e5376def2e2f386
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-11-18 00:06:41 +0100

    Initial Commit

---
+++ android/app/src/main/kotlin/com/sergiuwaxmann/codeitapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sergiuwaxmann.codeitapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CodeITApp());
+}
+
+class CodeITApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
